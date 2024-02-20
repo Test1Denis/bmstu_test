1a2ca53f4f99b81af4478094d4eadaa51b9378b2
Denis <ddd@ddd.com>
2024-02-20 14:30:34 +0300

    macros: #ifdef, #ifndef, #endif

--- main.cpp
+++ main.cpp
@@ -1,28 +1,27 @@
 #include <iostream>
 
-#define PRINT(A) (std::cout << A)
+#define PRINT(A) (std::cout << (int)A << " : " << (char)A << std::endl)
+#define MULT(A, B) ((A) * (B))
+
+int mult(int a, int b) {
+	return a*b;
+}
 
 int main() {
-	PRINT(100);
-	
-
-	int b = 894277321;
-
-	b = 0xFF;
-	b &= ~(0x30 << 4);	//reset 3 bit
-	std::cout << std::hex << b << std::endl;
-	b |= (1 << 3);
-	std::cout << std::hex << b << std::endl;
-	return 0;
-
-	int count = 0;
-
-	for (int i = 0; i < sizeof(int) * 8; i++) {
-		count += (b & (1 << i)) >> i; //count += b & (1 << i) ? 1 : 0;
+#ifdef DEBUG
+	for (int i = 0; i < 256; i++) {
+		PRINT(i);
 	}
-
-	std::cout << "one bit in " << std::hex <<  b << " = " << count << std::endl;
-
+#endif
+#ifdef ABC
+	std::cout << "ABC is defined" << std::endl;
+	PRINT(MULT(2+2, 7));
+#endif
+#ifndef ABC
+	std::cout << "ABC is NOT defined" << std::endl;
+	PRINT(mult(2+2, 5));
+#endif
+	
 
     return 0;
 }
