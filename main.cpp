fe05b18592f6da313cb03954f91f50830c665b20
Denis <ddd@ddd.com>
2024-02-20 14:49:14 +0300

    argument of main func

--- main.cpp
+++ main.cpp
@@ -1,27 +1,15 @@
 #include <iostream>
 
-#define PRINT(A) (std::cout << (int)A << " : " << (char)A << std::endl)
-#define MULT(A, B) ((A) * (B))
+void test(int, int);
 
-int mult(int a, int b) {
-	return a*b;
+int main(int argc, char** argv) {
+	for (int i = 0; i < argc; i++) {
+		std::cout << i << " " << argv[i] << std::endl;
+	}
+	
+    return 0;
 }
 
-int main() {
-#ifdef DEBUG
-	for (int i = 0; i < 256; i++) {
-		PRINT(i);
-	}
-#endif
-#ifdef ABC
-	std::cout << "ABC is defined" << std::endl;
-	PRINT(MULT(2+2, 7));
-#endif
-#ifndef ABC
-	std::cout << "ABC is NOT defined" << std::endl;
-	PRINT(mult(2+2, 5));
-#endif
-	
+void test(int a, int b) {
 
-    return 0;
 }
