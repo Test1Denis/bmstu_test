55c354825415d4055ebc1066c7fbbf431d2d61fc
Denis <ddd@ddd.com>
2024-02-20 13:31:33 +0300

    shift to left

--- main.cpp
+++ main.cpp
@@ -3,5 +3,24 @@
 
 int main() {
 
+	
+	int a = 1 >> 1;
+
+	std::cout << std::hex << a << std::endl;
+	return 0;
+
+	int b = 894277321;
+	int count = 0;
+
+	for (int i = 0; i < sizeof(int) * 8; i++) {
+		count += b & (1 << i) ? 1 : 0;
+
+		//std::cout << std::dec << i << " : " << (unsigned int)(1 << i)  << "\t" 
+		//			<< std::hex << (1 << i) << std::endl;
+	}
+
+	std::cout << "one bit in " << std::hex <<  b << " = " << count << std::endl;
+
+
     return 0;
 }
