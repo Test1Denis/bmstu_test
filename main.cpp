3601450d6a9adc1b94aea0eca208ebc3e996c7d8
Denis <ddd@ddd.com>
2024-02-20 13:43:03 +0300

    move bits.... left and right

--- main.cpp
+++ main.cpp
@@ -3,17 +3,12 @@
 
 int main() {
 
-	
-	int a = 1 >> 1;
-
-	std::cout << std::hex << a << std::endl;
-	return 0;
-
 	int b = 894277321;
 	int count = 0;
 
 	for (int i = 0; i < sizeof(int) * 8; i++) {
-		count += b & (1 << i) ? 1 : 0;
+//		count += b & (1 << i) ? 1 : 0;
+		count += (b & (1 << i)) >> i;
 
 		//std::cout << std::dec << i << " : " << (unsigned int)(1 << i)  << "\t" 
 		//			<< std::hex << (1 << i) << std::endl;
