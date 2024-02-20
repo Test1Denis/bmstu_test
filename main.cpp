e957e492d1f2b429909206627d3cf857aee0fd13
Denis <ddd@ddd.com>
2024-02-20 13:52:53 +0300

    set/reset bits

--- main.cpp
+++ main.cpp
@@ -4,14 +4,18 @@
 int main() {
 
 	int b = 894277321;
+
+	b = 0xFF;
+	b &= ~(1 << 3);	//reset 3 bit
+	std::cout << std::hex << b << std::endl;
+	b |= (1 << 3);
+	std::cout << std::hex << b << std::endl;
+	return 0;
+
 	int count = 0;
 
 	for (int i = 0; i < sizeof(int) * 8; i++) {
-//		count += b & (1 << i) ? 1 : 0;
-		count += (b & (1 << i)) >> i;
-
-		//std::cout << std::dec << i << " : " << (unsigned int)(1 << i)  << "\t" 
-		//			<< std::hex << (1 << i) << std::endl;
+		count += (b & (1 << i)) >> i; //count += b & (1 << i) ? 1 : 0;
 	}
 
 	std::cout << "one bit in " << std::hex <<  b << " = " << count << std::endl;
