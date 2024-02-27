36238df92ce3ba936f8d8d85aa131fbd7077fc5c
Denis <ddd@ddd.com>
2024-02-27 12:57:21 +0300

    switch, and selection sort

--- main.cpp
+++ main.cpp
@@ -1,15 +1,64 @@
+#include <algorithm>
 #include <iostream>
+#include <stdlib.h>
 
-void test(int, int);
+void GetA_doitany(int a) {
+	switch(a) {
+		case 0 :
+			std::cout << "hi " << std::hex << a;
+		break;
+		case 1 :
+			std::cout << "hello " << std::hex << a;
+		break;
+		
+		case 2 :
+		case 3 :
+		case 4 :
+		case 5 :
+			std::cout << "privet " << std::hex << a;
+		break;
+
+		default:
+			std::cout << "hren " << std::hex << a;
+		break;
+
+	}
+
+	std::cout << std::endl;
+}
+
+void BubbleSort(int* ar, int size) {
+	std::cout << "ona rabotaet " << __func__ << std::endl;
+
+}
+
+int GetMaxInd(cosnt int* ar, int size) {
+	int indMax = 0;
+	
+	for (int i = 1; i < size; i++) {
+		indMax = (ar[i] < ar[indMax]) ? indMax : i;
+	}
+
+	return indMax;
+}
+
+void SelectionSort(int *ar, int size) {
+	for (int i = 0; i < size; i++) {
+		int ind = GetMaxInd(&ar[i], size - i) + i;
+		std::swap(ar[ind], ar[i]);
+	}
+}
+
 
 int main(int argc, char** argv) {
-	for (int i = 0; i < argc; i++) {
-		std::cout << i << " " << argv[i] << std::endl;
+	int a = -1;
+	if (argc > 1) {
+//		itoa -- convert integer to string
+//	atoi -- convert str to int
+		a = atoi(argv[1]);
 	}
-	
+	GetA_doitany(a);
+		
     return 0;
 }
 
-void test(int a, int b) {
-
-}
