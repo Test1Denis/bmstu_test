1a9047f2b7fefa3189344494afbcf5b6ee5bcd2d
Denis <ddd@ddd.com>
2024-02-27 13:17:39 +0300

    ptr function

--- main.cpp
+++ main.cpp
@@ -28,8 +28,7 @@ void GetA_doitany(int a) {
 }
 
 void BubbleSort(int* ar, int size) {
-	std::cout << "ona rabotaet " << __func__ << std::endl;
-
+	std::cout << "ona ploxo rabotaet " << __func__ << std::endl;
 }
 
 int GetMaxInd(const int* ar, int size) {
@@ -43,10 +42,31 @@ int GetMaxInd(const int* ar, int size) {
 }
 
 void SelectionSort(int *ar, int size) {
+	std::cout << __FILE__ << " : " << __func__ << std::endl;
 	for (int i = 0; i < size; i++) {
 		int ind = GetMaxInd(&ar[i], size - i) + i;
 		std::swap(ar[ind], ar[i]);
 	}
+}
+
+void TestSort(void (*sort)(int*, int), const char* func_name) {
+	std::cout << func_name << " start test" << std::endl;
+	{
+		int ar[17] = {17,1,2,9,0,-1,6,6,2,8,7,3,6,6,6,9000, 9};
+		sort(ar, 17);
+		std::cout << std::dec << "\t\t";
+		for (int i = 0; i < 17; std::cout << ar[i++] << " ");
+		std::cout << std::endl;
+	}
+
+}
+
+void QuickSort(int*, int) {
+	std::cout << __func__ << std::endl;
+}
+
+void MergeSort(int*, int) {
+	std::cout << __func__ << std::endl;
 }
 
 
@@ -59,13 +79,17 @@ int main(int argc, char** argv) {
 	}
 	GetA_doitany(a);
 
-	int ar[17] = {17,1,2,9,0,-1,6,6,2,8,7,3,6,6,6,9000, 9};
-	SelectionSort(&ar[0], 17);
-
-	std::cout << std::dec;
-
-	for (int i = 0; i < 17; std::cout << ar[i++] << " ");
-	std::cout << std::endl;
+	void (*sorts[])(int*, int) = {SelectionSort, BubbleSort, QuickSort, MergeSort};
+	char* name_sorts[] = {"SelectionSort", "BubbleSort", "QuickSort", "MergeSort"};
+	for (int i = 0; i < 4; i++) {
+		TestSort(sorts[i], name_sorts[i]);
+	}
+/*
+	TestSort(SelectionSort, "SelectionSort");
+	TestSort(BubbleSort, "BubbleSort");
+	TestSort(QuickSort, "QuickSort");
+	TestSort(MergeSort, "MergeSort");
+*/
 		
     return 0;
 }
