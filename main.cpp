04b71e50e8d5527865a3e243408c0b376b4f0ed7
Denis <ddd@ddd.com>
2024-03-05 12:48:03 +0300

    bormatuxa

--- main.cpp
+++ main.cpp
@@ -1,10 +1,29 @@
 #include <algorithm>
 #include <iostream>
 #include <fstream>
+#include <cstring>
+//#include <string.h>
+
+
+char myStrg[256];
 
 int main(int argc, char** argv) {
 
+	char* myStr = new char [256];
+	
+	strcpy(myStr, "123456098321");
+	strcpy(myStr, "test");
+	myStr[strlen(myStr)] = 0x33;
+
+	for (int i = 0; i < 256; myStrg[i] = myStrg[i] == 0x00 ? ' ' : myStrg[i], i++);
+
+	size_t size_str = strlen(myStr);
+//	std::cout << myStr << " " << size_str << std::endl;
+	
+	size_str = strlen(myStrg);
+	std::cout << myStrg << " " << size_str << std::endl;
 
     return 0;
 }
+
 
