78320542ded96fd44a55916000881ed22f7ae511
Denis <ddd@ddd.com>
2024-03-13 09:56:19 +0300

    lambda, test sort, ptr function

--- main.cpp
+++ main.cpp
@@ -6,13 +6,19 @@
 
 #include "sorts.h"
 
+//int global = 100;
+
+bool temp_comp(int a, int b) {
+	return a > b;
+}
+
 void BubbleSort(int* ar, size_t size, bool (*comp)(int, int) = [](int a, int b){return a < b;}) {
 	int i = 0;
 	int j = 0;
 
 	while (j < size) {
 		i = 0;
-		while (i < size - 1) {
+		while (i < size - 1 - j) {
 			if (comp(ar[i], ar[i+1])) {
 				std::swap(ar[i], ar[i+1]);
 			}
@@ -52,6 +58,10 @@ void Test_Sort() {
 
 }
 
+void hard_function() {
+
+}
+
 int main(int argc, char** argv) {
 
 	Test_Sort();	
@@ -59,10 +69,17 @@ int main(int argc, char** argv) {
 	const int SIZE = 10;
 	int ar[SIZE] = {1,-2,3,0,-1,6,21,2,1,7};
 
+
 	for (int i = 0; i < SIZE; std::cout << ar[i++] << " ");
 	std::cout << std::endl;
 
-	BubbleSort(ar, SIZE, [](int a, int) {return (a % 2) == 0;});
+	auto pf = [](int a, int b){ 
+		static int count = 0;
+		std::cout << count++ << " : " << a << " " << b << std::endl;
+		return a > b;
+	};
+
+	BubbleSort(ar, SIZE, pf ); 
 
 	for (int i = 0; i < SIZE; std::cout << ar[i++] << " ");
 	std::cout << std::endl;
